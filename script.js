50cb115c4c5de76dc7e425262c7ec966c031c678
Wendryo S <wendryo.sales@gmail.com>
2021-12-10 13:37:02 -0300

    requisitos finais

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
 let penColour = 'black';
 
-// paleta de cores
-const colors = [/* "FFDBBB","FFD6BD","FFD5C4","FECDBF","FFC8C3","FFC3C2","FFBFC3","FFBAC4","FFB7C5","F5B8C1","F3AEC3","F3A6C6","F7A1C8","EF97C7","DD8CC6","D488C6","CB85C3","A776BB","976FB6", */"8569B4","7762B1","715BA4", "000000"];
+// paleta de cores predileta
 
-colors.reverse()
+const colorsFavorites =  ["FFFFFF","FFDBBB","FFD6BD","FFD5C4","FECDBF","FFC8C3","FFC3C2","FFBFC3","FFBAC4","FFB7C5","F5B8C1","F3AEC3","F3A6C6","F7A1C8","EF97C7","DD8CC6","D488C6","CB85C3","A776BB","976FB6","8569B4","7762B1","715BA4"]
+
+colorsFavorites.reverse() // array de trás pra frente
+
+//gera cor aleatoria ideia retirada do https://wallacemaxters.com.br/
+function gerarCor() {
+    let r = parseInt(Math.random() * 255);
+    let g = parseInt(Math.random() * 255);
+    let b = parseInt(Math.random() * 255);
+
+    return `rgba(${r}, ${g}, ${b})`;
+}
+
+//cores aleatorias
+ 
+let colors = ['black'];
+for (let i = 0; i < 3; i += 1){
+    let cor = gerarCor()
+    colors.push(cor)
+}
 
 function palletOfColors () {
     let palette = document.querySelector('#color-palette');
+    let paletteFavorite = document.querySelector('#color-palette-favorite');
 
     // percorre e cria cada cor da paleta e tira e coloca a classe 'selected'
     for(let i = 0; i < colors.length; i +=1 ) {
         let divColor
         if (colors[i] == "000000"){
             divColor = createPixel('color selected');
-            divColor.style.backgroundColor = '#' + colors[i];
+            divColor.style.backgroundColor = colors[i];
             palette.appendChild(divColor);
             divColor.addEventListener("click", selection);
             divColor.addEventListener("click",coletaCor);
         } else{
             divColor = createPixel('color');
-            divColor.style.backgroundColor = '#' + colors[i];
+            divColor.style.backgroundColor = colors[i];
             palette.appendChild(divColor);
             divColor.addEventListener("click", selection);
             divColor.addEventListener("click",coletaCor);
         }
     }
+
+    // faz a mesma coisa porém para as minhas cores fixas
+    for(let i = 0; i < colorsFavorites.length; i +=1 ) {
+        let divColor1
+        if (colors[i] == "000000"){
+            divColor1 = createPixel('color selected');
+            divColor1.style.backgroundColor = "#" + colorsFavorites[i];
+            paletteFavorite.appendChild(divColor1);
+            divColor1.addEventListener("click", selection);
+            divColor1.addEventListener("click",coletaCor);
+        } else{
+            divColor1 = createPixel('color');
+            divColor1.style.backgroundColor = "#" + colorsFavorites[i];
+            paletteFavorite.appendChild(divColor1);
+            divColor1.addEventListener("click", selection);
+            divColor1.addEventListener("click",coletaCor);
+        }
+    }
 }
 palletOfColors()
 
+// Variavel que pega a cor selecionada
 function coletaCor(){
     penColour = this.style.backgroundColor
     console.log(penColour)
 }
 
 
-// seleciona a cor desejada
+//  Verifica se a classe selected está atuando
 function selection (event) {
     let todasCores = document.getElementsByClassName('color');
     let corAtual = event.target;
@@ -58,8 +96,6 @@ function createPixel(className) {
 
 let botaoVqv = document.querySelector('#generate-board');
 let value = 5;
-
-/* botaoVqv.addEventListener("click", reset) */
 
 botaoVqv.addEventListener("click", function (){
     reset ()
@@ -112,12 +148,25 @@ function preenchePixelBoard (largura){
             let divPixel = createPixel("pixel");
             linha.appendChild(divPixel)
             divPixel.addEventListener('click', changeColor)
+            divPixel.addEventListener('mousedown', function () {
+                clicado = true;
+            });
+              
+            divPixel.addEventListener('mouseup', function () {
+                clicado = false;
+            });
+              
+            divPixel.addEventListener('mouseover', function () {
+                if (clicado) {
+                  this.style.backgroundColor = penColour;
+                }
+            });
         }
     }
 }
 
 
-// troca a classe do elemento clicado mudando a cor
+// troca a cor do elemento clicado
 function changeColor (event){
     event.target.style.backgroundColor = penColour;
 }
